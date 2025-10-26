6107f13d28a64e026d31a0a5a73504778e925e10
reynaldo0 <reynaldooyusellino@gmail.com>
2025-10-26 09:06:55 +0700

    feat : 2 page func

---
+++ app/pemilik/page.tsx
@@ -0,0 +1,7 @@
+export default function PemilikPage() {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-emerald-50 dark:bg-black text-black dark:text-white">
+            <h1 className="text-3xl font-semibold">Halaman Pemilik</h1>
+        </div>
+    );
+}
---
+++ app/siswa/page.tsx
@@ -0,0 +1,7 @@
+export default function SiswaPage() {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-blue-50 dark:bg-black text-black dark:text-white">
+            <h1 className="text-3xl font-semibold">Halaman Siswa</h1>
+        </div>
+    );
+}
